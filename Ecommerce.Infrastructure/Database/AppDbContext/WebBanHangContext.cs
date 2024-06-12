98d3d457ecc30ed3225a8e182c067276a512b213
Vann Trongg <vantrongvt1310@gmail.com>
2024-06-13 00:14:48 +0700

    comit

--- Ecommerce.Infrastructure/Database/AppDbContext/WebBanHangContext.cs
+++ Ecommerce.Infrastructure/Database/AppDbContext/WebBanHangContext.cs
@@ -47,7 +47,7 @@ namespace Ecommerce.Infrastructure.Database.AppDbContext
         {
             // Đông
 
-            //optionsBuilder.UseSqlServer("Data Source=DESKTOP-V6M0EF7\\SQLEXPRESS;Initial Catalog=WebBanHang;Integrated Security=True; TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-V6M0EF7\\SQLEXPRESS;Initial Catalog=WebBanHang;Integrated Security=True; TrustServerCertificate=True");
 
 
             // Hoàng
@@ -60,7 +60,7 @@ namespace Ecommerce.Infrastructure.Database.AppDbContext
             //optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=WebBanHang;Integrated Security=True; TrustServerCertificate=True");
 
             // Trọng
-            optionsBuilder.UseSqlServer("Server=vantrong\\SQLEXPRESS;Database=WebBanHang;Trusted_Connection=True;TrustServerCertificate=True");
+            //optionsBuilder.UseSqlServer("Server=vantrong\\SQLEXPRESS;Database=WebBanHang;Trusted_Connection=True;TrustServerCertificate=True");
 
             // Phong
             // BoptionsBuilder.UseSqlServer("Data Source=PHONGKEDAY2\\PHONGKE2004;Initial Catalog=WebBanHang;Integrated Security=True; TrustServerCertificate=True");
