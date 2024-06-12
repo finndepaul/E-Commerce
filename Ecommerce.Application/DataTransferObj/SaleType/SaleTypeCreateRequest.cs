c7ff797f8ddf77ec6a50fdeadbde7e7acfef56bd
phongke004 <phongx543@gmail.com>
2024-06-12 13:51:45 +0700

    saletype commit

---
+++ Ecommerce.Application/DataTransferObj/SaleType/SaleTypeCreateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.DataTransferObj.SaleType
+{
+    public class SaleTypeCreateRequest
+    {
+
+        public Guid SaleId { get; set; }
+        public string SaleTypeName { get; set; }
+    }
+}
