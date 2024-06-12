90504a954b2f570d673c92a6376a6421f44d551c
dong bui <dongbui.work@gmail.com>
2024-06-12 23:29:33 +0700

    product management

---
+++ Client/IServices/IAdminService.cs
@@ -0,0 +1,11 @@
+﻿using Ecommerce.Application.DataTransferObj.Products;
+using Ecommerce.Application.ValueObj.Pagination;
+
+namespace Client.IServices
+{
+    public interface IAdminService
+    {
+        Task<PaginationResponse<ProductDTO>> GetAll();
+        Task<bool> AcceptionProduct(Guid productID);
+    }
+}
---
+++ Client/Pages/Admin/ProductManagement.razor.cs
@@ -0,0 +1,39 @@
+﻿using Client.Dialog;
+using Client.IServices;
+using Ecommerce.Application.DataTransferObj.Products;
+using Ecommerce.Application.ValueObj.Pagination;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Client.Pages.Admin
+{
+    public partial class ProductManagement
+    {
+        [Inject] IAdminService _ser { get; set; }
+
+        public PaginationResponse<ProductDTO> list;
+
+        private MudTable<ProductDTO> _table; // để pagination
+        protected override async Task OnInitializedAsync()
+        {
+            list = await _ser.GetAll();
+            
+        }
+        public async Task DuyetSP(Guid id)
+        {
+            await _ser.AcceptionProduct(id);
+            await LoadTableData();
+        }
+        private async Task LoadTableData()
+        {
+            var newData = await _ser.GetAll();
+            list = newData;
+        }
+        private void PageChanged(int i)
+        {
+            _table.NavigateTo(i - 1);
+        }
+
+        
+    }
+}
